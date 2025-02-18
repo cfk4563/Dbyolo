b7392f2910d1e170bb55db5f72cd895209303259
chen3060 <1871471875@qq.com>
2025-02-18 21:42:42 +0800

    init

---
+++ train.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+
+
+if __name__ == '__main__':
+
+    model = YOLO("yolov8n.yaml").load('yolov8n.pt')
+    results = model.train(data="LROC.yaml",epochs=10, batch=8, imgsz=640)
+
